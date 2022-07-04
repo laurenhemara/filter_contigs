82bf573ed4ed66f99836e76ece42f33536d222c6
laurenhemara <80224188+laurenhemara@users.noreply.github.com>
2022-07-05 10:51:58 +1200

    Update filter_contigs.py

--- filter_contigs.py
+++ filter_contigs.py
@@ -3,7 +3,7 @@ import sys
 from Bio import SeqIO
 min_length, fasta_file_path = sys.argv[1:]
 with open(fasta_file_path.replace('fa', 'filter{}.fa'.format(min_length)), 'w') as filtered_fasta:
-	with open(fasta_file_path, 'rU') as input_fasta:
+	with open(fasta_file_path, 'r') as input_fasta:
 		def filtered_contigs_generator(min):
 			for contig in SeqIO.parse(input_fasta, 'fasta'):
 				if len(contig) >= min:
